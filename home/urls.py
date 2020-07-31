031ba2a3920c8bca13a288c05bb825cd0dc8d9c8
Priyansh Garg <priyanshgarg30@gmail.com>
2020-07-31 20:56:12 +0530

    Dashboard AJAX implemented

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,9 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('road_details/<str:slug>', views.road_details, name='road_details'),
+
+    # AJAX
+    path('ajax_state_changed/', views.ajax_state_changed, name='ajax_state_changed'),
+    path('ajax_district_changed/', views.ajax_district_changed, name='ajax_district_changed'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,77 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import Road, Image, Issue, IssueDetail
 
 # Create your views here.
 def index(request):
+    if request.user.is_authenticated:
+        return redirect('home:dashboard')
     return render(request, 'home/index.html')
 
+@login_required
 def dashboard(request):
-    return render(request, 'home/dashboard.html')
+    state = request.GET.get('state', None)
+    district = request.GET.get('district', None)
+    block = request.GET.get('block', None)
+
+    roads = Road.objects.all()
+    states = roads.values_list('state', flat=True).distinct().order_by('state')
+
+    if state and district and block:
+        roads = roads.filter(state=state)
+        districts = roads.values_list('district', flat=True).distinct().order_by('district')
+        roads = roads.filter(district=district)
+        blocks = roads.values_list('block', flat=True).distinct().order_by('block')
+        roads = roads.filter(block=block)
+        if roads.exists():
+            context = {
+                'states': states,
+                'districts': districts,
+                'blocks': blocks,
+                'roads': roads,
+
+                'selected_state': state,
+                'selected_district': district,
+                'selected_block': block,
+            }
+            return render(request, 'home/dashboard.html', context)
+        else:
+            return redirect('home:dashboard')
+
+    context = {
+        'states': states,
+    }
+    return render(request, 'home/dashboard.html', context)
+
+@login_required
+def road_details(request, slug):
+    return render(request, 'home/road_details.html')
+
+def ajax_state_changed(request):
+    state = request.GET.get('state', None)
+    data = {}
+
+    if state:
+        roads = Road.objects.filter(state=state)
+        districts = roads.values_list('district', flat=True).distinct().order_by('district')
+
+        for district in districts:
+            data[district] = district
+
+    return JsonResponse(data)
+
+def ajax_district_changed(request):
+    state = request.GET.get('state', None)
+    district = request.GET.get('district', None)
+    data = {}
+
+    if state and district:
+        roads = Road.objects.filter(state=state, district=district)
+        blocks = roads.values_list('block', flat=True).distinct().order_by('block')
+
+        for block in blocks:
+            data[block] = block
+
+    return JsonResponse(data)
