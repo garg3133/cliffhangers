37a9541fb2702ce04aa32997e641d18a3c3f52d4
Priyansh Garg <priyanshgarg30@gmail.com>
2020-08-02 02:04:02 +0530

    api urls update

--- home/api/urls.py
+++ home/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create_road/', views.create_road, name='api_create_road'),
     path('update_road_image/', views.update_road_image, name='api_update_road_image'),
     path('update_road_pci/', views.update_road_pci, name='api_update_road_pci'),
 ]
